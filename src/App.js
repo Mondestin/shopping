b8d1fca7a5d84e5b451846860e84cefb7e30e45c
johan-mickael <m.johan.rkt@gmail.com>
2022-03-10 21:43:59 +0100

    Adding routes to navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import * as React from 'react';
-import Navbar from './components/NavBar';
-import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import * as React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Shopping from "./components/Shopping";
+import NewItem from "./pages/newitem";
 
 function App() {
   return (
-    <div className="App"
-      style={{
-      }}>
+    <div className="App" style={{}}>
       <Router>
         <Navbar />
+        <Routes>
+          <Route path="/shoppings" element={<Shopping />} />
+          <Route path="/items" element={<NewItem />} />
+        </Routes>
       </Router>
-
-    </div >
-
+    </div>
   );
 }
 export default App;
-
