15b84cb0d9cd2e31870e198efe21e8085a7d7ece
unknown <m.johan.rkt@gmail.com>
2022-05-24 23:51:10 +0200

    Done with shopping form validation

--- src/components/Shopping/Form/NewShoppingForm.js
+++ src/components/Shopping/Form/NewShoppingForm.js
@@ -1,62 +1,71 @@
-import { Card, CardActions, CardContent, Grid, TextField, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Grid, TextField, Typography } from '@mui/material';
 import { addDoc, collection, Timestamp } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
-import CreateShoppingButton from '../Button/CreateShoppingButton';
 import db from "../../../firebase_config";
+import { useForm } from "react-hook-form";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 const NewShoppingForm = () => {
-    const [name, setName] = useState("")
-    const [description, setDescription] = useState("")
+    const { register, handleSubmit, watch, setValue, formState: { errors } } = useForm();
 
-    const saveItem = async (e) => {
-        e.preventDefault();
+    const saveItem = async () => {
         await addDoc(collection(db, "shoppings"), {
-            name: name,
-            description: description,
+            name: watch("name"),
+            description: watch("description"),
             date: Timestamp.fromDate(new Date()),
         });
+        resetInputValue()
     };
 
-
+    const resetInputValue = () => {
+        setValue("name", "")
+        setValue("description", "")
+    }
 
     return (
-        <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-                <Typography variant="h6" color="text.secondary" gutterBottom>
-                    Create new shopping
-                </Typography>
-                <Typography variant="body">
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                label="Name"
-                                autoFocus
-                                name="name"
-                                onChange={(e) => {
-                                    setName(e.target.value)
-                                }}
-                            />
+        <form onSubmit={handleSubmit(saveItem)}>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography variant="h6" color="text.secondary" gutterBottom>
+                        Create new shopping
+                    </Typography>
+
+                    <Typography variant="body">
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={12}>
+                                <TextField
+                                    fullWidth
+                                    label="Name"
+                                    autoFocus
+                                    {...register("name", { required: "Please fill the name." })}
+                                    helperText={errors.name && errors.name.message}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={12}>
+                                <TextField
+                                    fullWidth
+                                    label="Description"
+                                    name="description"
+                                    {...register("description", { required: "Please fill the description." })}
+                                    helperText={errors.description && errors.description.message}
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                label="Description"
-                                name="description"
-                                onChange={(e) => {
-                                    setDescription(e.target.value)
-                                }}
-                            />
-                        </Grid>
-                    </Grid>
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <CreateShoppingButton name={name} saveItem={saveItem} description={description} />
-            </CardActions>
-        </Card>
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button
+                        type="submit"
+                        variant="outlined"
+                        color="primary"
+                        disableElevation
+                        startIcon={<AddShoppingCartIcon />}
+                        fullWidth
+                    >
+                        Add
+                    </Button>
+                </CardActions>
+            </Card>
+        </form>
     );
 }
 
--- src/components/Shopping/Form/UpdateShoppingForm.js
+++ src/components/Shopping/Form/UpdateShoppingForm.js
@@ -1,85 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardActions, CardContent, Grid, TextField, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Grid, TextField, Typography } from '@mui/material';
 import { useNavigate, useOutletContext, useParams } from "react-router-dom";
-import UpdateShoppingButton from "../Button/UpdateShoppingButton";
 import { doc, updateDoc } from "firebase/firestore";
 import db from "../../../firebase_config";
+import { useForm } from "react-hook-form";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const UpdateShoppingForm = () => {
   const [shoppings, setShoppings] = useOutletContext();
   const navigate = useNavigate();
   const { id } = useParams();
+  const [isLoading, setLoading] = useState(false)
   let shopping = shoppings.filter((shopping) => shopping.id === id)[0].data();
+  const { register, handleSubmit, watch, setValue, clearErrors, formState: { errors } } = useForm();
 
-  const [name, setName] = useState(shopping.name)
-  const [description, setDescription] = useState(shopping.description)
-  const [nameError, setNameError] = useState("")
-  const [descriptionError, setDescriptionError] = useState("")
-  const [verifyName, setVerifyName] = useState(true)
-  const [verifyDescription, setVerifyDescription] = useState(true)
 
-  const handleNameInput = (value) => {
-    setName(value)
-    if (value !== "") {
-      setVerifyName(true)
-      setNameError("")
-    }
-    else {
-      setVerifyName(false)
-      setNameError("Please fill the name.")
-    }
-  }
 
-  const handleDescriptionInput = (value) => {
-    setDescription(value)
-    if (value !== "") {
-      setVerifyDescription(true)
-      setDescriptionError("")
-    }
-    else {
-      setVerifyDescription(false)
-      setDescriptionError("Please fill the description.")
-    }
-  }
-
-  const updateShopping = async (e) => {
-    e.preventDefault();
-    if (handleSubmit()) {
-      const shoppingRef = doc(db, "shoppings", id);
-      await updateDoc(shoppingRef, {
-        name: name,
-        description: description
-      })
-      // Redirecting to the create shopping form 
-      // navigate('/shoppings/new')
-    }
-  };
-
-  const handleInputChange = ({ target }) => {
-    switch (target.name) {
-      case "name":
-        handleNameInput(target.value)
-        break;
-      case "description":
-        handleDescriptionInput(target.value)
-        break;
-      default:
-        break;
-    }
-  }
-
-  const handleSubmit = () => {
-    if (name === "") {
-      setNameError("Please fill the name.")
-      setVerifyName(false)
-    }
-    if (description === "") {
-      setDescriptionError("Please fill the description.")
-      setVerifyDescription(false)
-    }
-    if (verifyName && verifyDescription)
-      return true
-    return false
+  const updateShopping = async () => {
+    const shoppingRef = doc(db, "shoppings", id);
+    await updateDoc(shoppingRef, {
+      name: watch("name"),
+      description: watch("description")
+    })
+    // Redirecting to the create shopping form 
+    navigate('/shoppings/new')
   }
 
   /**
@@ -87,58 +31,59 @@ const UpdateShoppingForm = () => {
    * Repopulate the edit form with the current shopping clicked to edit
    */
   useEffect(() => {
-    setName(shopping.name)
-    setDescription(shopping.description)
-    setVerifyName(true)
-    setVerifyDescription(true)
-    setNameError("")
-    setDescriptionError("")
+    clearErrors()
+    setValue("name", shopping.name)
+    setValue("description", shopping.description)
   }, [id])
 
+
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography variant="h6" color="text.secondary" gutterBottom>
-          Edit shopping informations
-        </Typography>
-        <Typography variant="body">
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                error={!verifyName}
-                required
-                fullWidth
-                name="name"
-                label="Name"
-                value={name}
-                autoFocus
-                onChange={(e) => {
-                  handleInputChange(e)
-                }}
-                helperText={nameError}
-              />
+    <form onSubmit={handleSubmit(updateShopping)}>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h6" color="text.secondary" gutterBottom>
+            Edit shopping informations
+          </Typography>
+          <Typography variant="body">
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  error={errors.name}
+                  fullWidth
+                  name="name"
+                  label="Name"
+                  autoFocus
+                  {...register("name", { required: "Please fill the name." })}
+                  helperText={errors.name && errors.name.message}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  error={errors.description}
+                  fullWidth
+                  name="description"
+                  label="Description"
+                  {...register("description", { required: "Please fill the description." })}
+                  helperText={errors.description && errors.description.message}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                error={!verifyDescription}
-                required
-                fullWidth
-                name="description"
-                label="Description"
-                value={description}
-                onChange={(e) => {
-                  handleInputChange(e)
-                }}
-                helperText={descriptionError}
-              />
-            </Grid>
-          </Grid>
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <UpdateShoppingButton shopping={{ id, name, description }} updateShopping={updateShopping} />
-      </CardActions>
-    </Card>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="outlined"
+            color="primary"
+            disableElevation
+            startIcon={<ShoppingCartIcon />}
+            fullWidth
+            type="submit"
+          >
+            Update
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
   );
 };
 
